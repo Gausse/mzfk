763626f9cd1b3103ba659bfdb565cb67ddb4fa86
Igor Svyrydiuk <Igor Svyrydiuk>
2016-03-19 12:58:31 +0200

    * Add insert/search/remove/at/sort/show impl

--- code/List/List.hpp
+++ code/List/List.hpp
@@ -15,6 +15,43 @@ class List
     Node *p_begin, *p_end;
     int m_size;
 public:
+    struct iterator
+    {
+        Node* node;
+        iterator() : node(NULL) {}
+        iterator(Node* l_node) : node(l_node) {}
+
+        T& operator*() const
+        {
+            return node;
+        }
+        T* operator->() const
+        {
+            return *node;
+        }
+
+        iterator& operator++()
+        {
+            node = node->p_next;
+            return *this;
+        }
+        iterator operator++(int)
+        {
+            iterator* tmp = *this;
+            node = node->p_next;
+            return tmp;
+        }
+
+        friend bool operator==(iterator a, iterator b)
+        {
+            return a.node == b.node;
+        }
+        friend bool operator!=(iterator a, iterator b)
+        {
+            return a.node != b.node;
+        }
+    };
+
     List(): p_begin(NULL), p_end(NULL), m_size(0)
     {
         std::cout << "Empty constructor was called " << this << std::endl;
@@ -52,9 +89,17 @@ public:
 
     void push_back(T data)
     {
-        Node* node = new Node(data, NULL, p_end);
-        p_end->p_next = node;
-        p_end = node;
+
+        Node* node = new Node(data, NULL, NULL);
+        if (m_size > 0)
+        {
+            node->p_prev = p_end;
+            p_end->p_next = node;
+            p_end = node;
+        } else if (m_size == 0)
+        {
+            p_begin = p_end = node;
+        }
         ++m_size;
         std::cout << "push_back put in the back of list with addr "
                   << node
@@ -63,29 +108,38 @@ public:
 
     void pop_back()
     {
-
-        Node* del = p_end;
-        if (m_size > 1)
+        if (m_size > 0)
         {
-            p_end->p_prev->p_next = NULL;
-            p_end = p_end->p_prev;
-        } else if (m_size == 1)
-        {
-            p_begin = NULL;
-            p_end   = NULL;
+            Node* del = p_end;
+            if (m_size > 1)
+            {
+                p_end->p_prev->p_next = NULL;
+                p_end = p_end->p_prev;
+            } else if (m_size == 1)
+            {
+                p_begin = NULL;
+                p_end   = NULL;
+            }
+            --m_size;
+            std::cout << "pop_back deletes node from the back of list with addr "
+                      << del
+                      << std::endl;
+            delete del;
         }
-        --m_size;
-        std::cout << "pop_back deletes node from the back of list with addr "
-                  << del
-                  << std::endl;
-        delete del;
     }
 
     void push_front(T data)
     {
         Node* node = new Node(data, p_begin, NULL);
-        p_begin->p_prev = node;
-        p_begin = node;
+        if (m_size > 0)
+        {
+            node->p_next = p_begin;
+            p_begin->p_prev = node;
+            p_begin = node;
+        } else if (m_size == 0)
+        {
+            p_begin = p_end = node;
+        }
         ++m_size;
         std::cout << "push_front put in front of the list with addr "
                   << node
@@ -94,64 +148,154 @@ public:
 
     void pop_front()
     {
-        Node* del = p_begin;
-        if (m_size > 1)
+        if (m_size > 0)
         {
-            p_begin->p_next->p_prev = NULL;
-            p_begin = p_begin->p_next;
-        } else if (m_size == 1)
-        {
-            p_begin = NULL;
-            p_end   = NULL;
+            Node* del = p_begin;
+            if (m_size > 1)
+            {
+                p_begin->p_next->p_prev = NULL;
+                p_begin = p_begin->p_next;
+            } else if (m_size == 1)
+            {
+                p_begin = NULL;
+                p_end   = NULL;
+            }
+            --m_size;
+            std::cout << "pop_front deletes node from the front of list with addr "
+                      << del
+                      << std::endl;
+            delete del;
         }
-        --m_size;
-        std::cout << "pop_front deletes node from the front of list with addr "
-                  << del
-                  << std::endl;
-        delete del;
     }
 
-    class iterator
+    iterator begin()
     {
-        Node* node;
-    public:
-        iterator() : node(NULL) {}
-        iterator(Node* l_node) : node(l_node) {}
+        return iterator(p_begin);
+    }
 
-        T& operator*() const
-        {
-            return node;
-        }
-        T* operator->() const
-        {
-            return *node;
-        }
+    iterator end()
+    {
+        return iterator(p_end);
+    }
 
-        iterator& operator++()
-        {
-            node = node->p_next;
-            return *this;
-        }
-        iterator operator++(int)
-        {
-            iterator* tmp = *this;
-            node = node->p_next;
-            return tmp;
-        }
+    iterator rbegin()
+    {
+        return iterator(p_end);
+    }
 
-        friend bool operator==(iterator a, iterator b)
+    iterator rend()
+    {
+        return iterator(p_begin);
+    }
+
+    void insert(T data, int place)
+    {
+        if (place > m_size)
         {
-            return a.node == b.node;
+            push_back(data);
+            return;
         }
-        friend bool operator!=(iterator a, iterator b)
+        iterator it = begin();
+        iterator null_it;
+        int index = 0;
+        while(it != null_it)
         {
-            return a.node != b.node;
+            if (index == place)
+            {
+                Node* node = new Node(data, it.node, it.node->p_prev);
+                it.node->p_prev->p_next = node;
+                it.node->p_prev = node;
+                ++m_size;
+                return;
+            }
+            ++it;
+            ++index;
         }
-    };
-//    void insert(Node* data, int place);
-//    Node* search(); //?????????????
-//    void deleteNode();
+    }
+    //ask about what return when search failed
+    iterator search(T data, bool(*qualifier)(T, T))
+    {
+        for(iterator it = begin(), l_end = end(); it != l_end; ++it)
+        {
+            if (qualifier(it.node->data, data))
+                return it;
+        }
+        return iterator();
+    }
+
+    void remove(int place)
+    {
+        if (place == 0)
+        {
+            pop_front();
+            return;
+        } else if (place == m_size - 1)
+        {
+            pop_back();
+            return;
+        }
+        if ((place <= m_size-1) && (place > 0))
+        {
+            int index = 1;
+            for(iterator it = ++begin(), l_end = end(); it != l_end; ++it)
+            {
+                if (index == place)
+                {
+                    it.node->p_prev->p_next = it.node->p_next;
+                    it.node->p_next->p_prev = it.node->p_prev;
+
+                    delete it.node;
+                    --m_size;
+                    return;
+                }
+                ++index;
+            }
+        }
+    }
+
+    iterator at(int place)
+    {
+        if (place < m_size)
+        {
+            int index = 0;
+            for(iterator it = begin(); it.node != NULL; ++it)
+            {
+                if (index == place)
+                {
+                    return it;
+                }
+                ++index;
+            }
+        }
+        return NULL;
+    }
+
+    void sort(bool(*qualifier)(T,T))
+    {
+        for (int i = 0; i < m_size; ++i)
+        {
+            Node nMin = *at(i).node;
+            int iMinInd = i;
+            for (int j = i + 1; j < m_size; ++j)
+            {
+                if (qualifier(at(j).node->data, nMin.data))
+                {
+                    nMin = *at(j).node;
+                    iMinInd = j;
+                }
+            }
+            at(iMinInd).node->data = at(i).node->data;
+            at(i).node->data = nMin.data;
+        }
+    }
+
+    void show(void (*shower)(T))
+    {
+        for(iterator it = begin(); it.node != NULL; ++it)
+        {
+            shower(it.node->data);
+        }
+    }
+
 //    void Sort();
-//    void show();
-//write iterator
 };
--- code/List/main.cpp
+++ code/List/main.cpp
@@ -1,10 +1,35 @@
 #include "core.h"
 #include "List.hpp"
 
+bool compare(int a, int b)
+{
+    return a == b;
+}
+
+bool desc(int a, int b)
+{
+    return a > b;
+}
+
+void shower(int a)
+{
+    std::cout << a << " " << std::endl;
+}
+
 int main()
 {
-    List<char> lt('a');
-    lt.pop_back();
+    List<int> lt;
+    for(int index = 0; index < 10; ++index)
+        lt.push_back(index);
+
+
+    lt.insert(15, 5);
+
+    lt.remove(5);
+
+    lt.sort(desc);
+
+    lt.show(shower);
     return 0;
 }
 
